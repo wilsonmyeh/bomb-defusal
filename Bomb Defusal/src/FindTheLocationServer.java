c24b584b9e4ab3c07c922937fbfca47800ac7bac
Blake Oler <oler@usc.edu>
2014-11-29 18:59:22 -0800

    Working on FindTheLocation

--- Bomb Defusal/src/FindTheLocationServer.java
+++ Bomb Defusal/src/FindTheLocationServer.java
@@ -1,9 +1,98 @@
+import java.util.Random;
+
+/*
+ * 
+ * //should only talk to THE server
+abstract class BaseMiniGameServer {
+	boolean active;
+	boolean solved;
+	int teamNumber;
+	int gameNumber;
+	Server server;
+	boolean kickable;
+	BaseMiniGameServer me = this;
+	Thread timer = new Thread() {
+		public void run() {
+			try {
+				Thread.sleep(15000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			kickable = true;
+			//server.sendCommand("kickable")
+			server.sendCommand(me,"KICK ME HARD"); 
+		}
+	};
+	
+	public BaseMiniGameServer() {
+		kickable = false;
+		active = false;
+	}
+	
+	
+	abstract void parseCommand(String command);
+		//if(start) { timer.start(); active = true; } or something liek that
+	
+	void sendCommand(String command){
+		server.sendCommand(this,command);
+	}
+	
+}
+ */
+
 class FindTheLocationServer extends BaseMiniGameServer{
 	private final int GAME_ID = 0;
+	private int currentX;
+	private int currentY;
+	private int desiredX;
+	private int desiredY;
+	private int currentPercent;
+	private int currentDistance;
+	private int maxDistance;
+	private Random rand;
+	
+	FindTheLocationServer(){
+		rand = new Random();
+	}
+	//Message possibilities
+	/*
+	 * location x y
+	 * 
+	 * 
+	 */
+	
+	
+	void setDesiredLocation(){
+		desiredX = rand.nextInt(500);
+		desiredY = rand.nextInt(400);
+		
+		int maxXDistance = desiredX;
+		if((500 - desiredX) > maxXDistance){
+			maxXDistance = 500 - desiredX;
+		}
+		
+		int maxYDistance = desiredY;
+		if((400 - desiredY) > maxYDistance){
+			maxYDistance = 500 - desiredY;
+		}
+		
+		maxDistance = Math.abs((int) Math.sqrt((double)((desiredX - maxXDistance) * (desiredX - maxXDistance) + (desiredY - maxYDistance) * (desiredY - maxYDistance))));
+	}
+	
+	void setCurrentLocation(int x, int y){
+		currentX = x;
+		currentY = y;
+		
+		currentDistance = Math.abs((int) Math.sqrt((double)((desiredX - currentX) * (desiredX - currentX) + (desiredY - currentY) * (desiredY - currentY))));
+		currentPercent= 100 - currentDistance/maxDistance;
+	}
+	
+	boolean checkWin(){
+		return (currentPercent >= 90);
+	}
 
 	@Override
 	void parseCommand(String command) {
 		// TODO Auto-generated method stub
-		
 	}
 }
--- Bomb Defusal/src/FindTheLocationSupervisor.java
+++ Bomb Defusal/src/FindTheLocationSupervisor.java
@@ -1,7 +1,105 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Random;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 
 class FindTheLocationSupervisor extends JPanel implements BaseMiniGameClient{
 	private final int GAME_ID = 0;
+	private Random rand;
+	private Image image;
+	private int currentX;
+	private int currentY;
+	private JButton finalize;
+	
+	public FindTheLocationSupervisor(){
+		rand = new Random();
+		this.addMouseListener(new MouseAdapter(){
+			public void mousePressed(MouseEvent e) {
+				int x = e.getX();
+				int y = e.getY();
+				if(x < 500 && y < 400){
+					currentX = x;
+					currentY = y;
+					updateCrosshairs();
+				}
+		    }
+		});
+		
+		currentX = -1;
+		currentY = -1;
+		setImage(-1);
+		
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		add(Box.createVerticalGlue());
+		finalize = new JButton("Finalize.");
+		finalize.setAlignmentX(CENTER_ALIGNMENT);
+		add(finalize);
+		add(Box.createVerticalStrut(50));
+	}
+	
+	public void updateCrosshairs(){
+		repaint();
+		//send message to server
+	}
+	
+	public void setImage(int notMe){
+		int imageIndex = -1;
+		String imageIndexStr;
+		String imageLocation;
+		if(notMe == -1){
+			imageIndex = rand.nextInt(5);
+			imageIndexStr = String.valueOf(imageIndex);
+			imageLocation = imageIndexStr + ".jpg";
+			image = new ImageIcon(imageLocation).getImage(); 
+		}
+		else{
+			while(imageIndex == -1){
+				imageIndex = rand.nextInt(5);
+				if(imageIndex == notMe){
+					imageIndex = -1;
+				}
+				else{
+					imageIndexStr = String.valueOf(imageIndex);
+					imageLocation = imageIndexStr + ".jpg";
+					image = new ImageIcon(imageLocation).getImage();
+				}
+			}
+		}
+		repaint();
+	}
+	
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		if(image != null){
+			g.drawImage(image, 0, 0, null);
+		}
+		if(currentX != -1 && currentY != -1){
+			g.setColor(Color.red);
+			g.drawLine(0, currentY, 500, currentY);
+			g.drawLine(currentX, 0, currentX, 400);	
+		}
+		
+	}
+	
+	public static void main(String[] args){
+		JFrame test = new JFrame();
+		test.setSize(500, 500);
+		JPanel testPanel = new FindTheLocationSupervisor();
+		test.add(testPanel);
+		
+		test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		test.setVisible(true);
+	}
 }
 
