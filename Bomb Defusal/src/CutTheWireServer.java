9dfa61f5ef07fba3c10d90cdcdf9a8ace8988418
Blake Oler <oler@usc.edu>
2014-12-01 22:05:57 -0800

    Fixed client team issues

--- Bomb Defusal/src/CutTheWireServer.java
+++ Bomb Defusal/src/CutTheWireServer.java
@@ -3,8 +3,8 @@ import java.util.Random;
 class CutTheWireServer extends BaseMiniGameServer {
 	private final int GAME_ID = 2;
 
-	public CutTheWireServer(Server s) {
-		super(s);
+	public CutTheWireServer(Server s, int teamNum) {
+		super(s, teamNum);
 		Random rand = new Random();
 		int randomNum = rand.nextInt(10);
 		//sendCommand( GAME_ID + "Random " + randomNum);
